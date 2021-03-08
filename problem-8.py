dea5fbd2faa33d0134402ce10c7e8437a4d99506
Andrew Smith <a.smithb21@gmail.com>
2021-03-08 12:57:11 -0600

    added a recursive solution

--- problem-8.py
+++ problem-8.py
@@ -32,3 +32,40 @@ class Solution:
         elif solution < -2**31:
             solution = -2**31
         return solution
+
+
+# Recursive Solution
+def step1(s):
+    if s:
+        if s[0] == ' ':
+            return step1(s[1:])
+        return step2(s)
+    else:
+        return 0
+
+def step2(s):
+    if s[0] == '-' or s[0] == '+':
+        return step3(s[1:],s[0])
+    return step3(s)
+
+def step3(s,solution=''):
+    if s:
+        if s[0] in '0123456789':
+            return step3(s[1:],solution+s[0])
+    return step4(solution)
+
+def step4(solution):
+    try:
+        if solution:
+            if int(solution)>2**31-1:
+                return 2**31-1
+            elif int(solution)<-2**31:
+                return -2**31
+            return int(solution)
+        return 0
+    except:
+        return 0
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        return step1(s)
