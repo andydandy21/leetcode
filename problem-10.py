3b51607722241d3e2036f1f348dae9b8d8a834de
Andrew Smith <a.smithb21@gmail.com>
2021-03-09 14:45:33 -0600

    problem-10 commit

---
+++ problem-10.py
@@ -0,0 +1,58 @@
+#Regular Expression Matching
+
+
+def regex(s,p):
+    if not p:
+        return p==s
+    else:
+        if not s:
+            if p[-1] == '*':
+                return regex(s, p[:-2])
+            return False
+        
+        elif p[-1] == s[-1] or p[-1] == ".":
+            return regex(s[:-1], p[:-1])
+        
+        elif p[-2:] == '.*':
+            if len(p)>2:
+                if not s:
+                    return False
+                else:
+                    if p[-4:-2] == '.*':
+                        return regex(s,p[:-2])
+                    elif p[-3] == s[-1] or p[-3]=='.':
+                        if regex(s,p[:-2]):
+                            return regex(s,p[:-2])
+                        else:
+                            return regex(s[:-1], p)
+                    elif p[-3] == '*':
+                        if regex(s,p[:-2]):
+                            return regex(s,p[:-2])
+                        else:
+                            return regex(s[:-1], p)
+                    else:
+                        return regex(s[:-1],p)
+            else:
+                return True
+        
+        elif p[-1] == '*':
+            if p[-2] == s[-1]:
+                if regex(s[:-1], p[:-2]):
+                    return regex(s[:-1], p[:-2])
+                else:
+                    if regex(s[:-1],p):
+                        return regex(s[:-1],p)
+                    else:
+                        return regex(s,p[:-2])
+                        
+            else:
+                return regex(s, p[:-2])
+        
+        else:
+            return False
+            
+            
+            
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        return regex(s,p)
